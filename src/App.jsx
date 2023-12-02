b15483e4984a36df0131005e236f83a2e1eff321
Shakil57375 <shakil57375@gmail.com>
2023-12-02 15:16:30 +0600

    metarial button  added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { Button } from "@mui/material";
 import "./App.css";
-
+import SettingsIcon from "@mui/icons-material/Settings";
+import AddIcon from "@mui/icons-material/Add";
 function App() {
   return (
     <>
       <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <Button
+        startIcon={<SettingsIcon />}
+        variant="contained"
+        color="secondary"
+        size="small"
+      >
+        Contained
+      </Button>
+      <Button startIcon={<AddIcon />} variant="outlined">
+        Outlined
+      </Button>
     </>
   );
 }
