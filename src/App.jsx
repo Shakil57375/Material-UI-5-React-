209faf07b2888d86f4162b32345a5ac27b856214
Shakil57375 <shakil57375@gmail.com>
2023-12-02 15:27:22 +0600

    unique  button  added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import { Button } from "@mui/material";
+import { Button, styled } from "@mui/material";
 import "./App.css";
 import SettingsIcon from "@mui/icons-material/Settings";
 import AddIcon from "@mui/icons-material/Add";
 function App() {
+  const BlueButton = styled(Button)({
+    backgroundColor: "skyblue",
+    color: "#000",
+    margin: 5,
+    "&hover": {
+      backgroundColor: "lightblue",
+    },
+    // when disabled it will work
+    "&:disabled": {
+      backgroundColor: "gray",
+      color: "white",
+    },
+  });
   return (
     <>
       <Button variant="text">Text</Button>
@@ -17,24 +30,7 @@ function App() {
       <Button startIcon={<AddIcon />} variant="outlined">
         Outlined
       </Button>
-      <Button
-        variant="contained"
-        sx={{
-          backgroundColor: "skyblue",
-          color: "#000",
-          margin: 5,
-          "&hover": {
-            backgroundColor: "lightblue",
-          },
-          // when disabled it will work
-          "&:disabled": {
-            backgroundColor: "gray",
-            color: "white",
-          },
-        }}
-      >
-        My Unique Button
-      </Button>
+      <BlueButton>My Unique Button</BlueButton>
     </>
   );
 }
